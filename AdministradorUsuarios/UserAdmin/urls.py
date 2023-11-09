e9eef1590430959cddc58d15dd9a92871725acee
0xGeN02 <manuelmateodgl02@gmail.com>
2023-11-09 12:07:56 +0100

    cambios proyecto

--- AdministradorUsuarios/UserAdmin/urls.py
+++ AdministradorUsuarios/UserAdmin/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for AdministradorArchivos project.
+URL configuration for ProyectoBaseDatos project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.2/topics/http/urls/
--- AdministradorUsuarios/UserAdmin/wsgi.py
+++ AdministradorUsuarios/UserAdmin/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for AdministradorArchivos project.
+WSGI config for ProyectoBaseDatos project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "AdministradorArchivos.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "ProyectoBaseDatos.settings")
 
 application = get_wsgi_application()
